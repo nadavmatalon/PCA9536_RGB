d11eaa20a2dcccec763a1894f44c6679b1adad1e
Nadav <nadav.matalon@gmail.com>
2016-10-30 17:15:06 +0000

    interrupt-based blink

--- examples/PCA9536_RGB_Blink/PCA9536_RGB_Blink.ino
+++ examples/PCA9536_RGB_Blink/PCA9536_RGB_Blink.ino
@@ -2,14 +2,14 @@
 
 PCA9536_RGB rgb(IO2, IO1, IO0, C_ANODE);
 
-const byte PIN_D2 = 2;
-
 volatile boolean switchFlag;
 
 void setup() {
-    pinMode(PIN_D2, INPUT_PULLUP);
-    switchFlag = digitalRead(PIN_D2) ? false : true;
-    attachInterrupt (digitalPinToInterrupt (PIN_D2), switchPressed, CHANGE);
+    DDRD &= ~(1 << DDD2); PORTD |= (1 << PORTD2);                               // pinMode(PIN_D2, INPUT_PULLUP)
+    switchFlag = !bitRead(PIND, 2);                                             // !digitalRead(PIN_D2)
+    EICRA |= (1 << ISC01);                                                      // Trigger INT0 on falling edge
+    EIMSK |= (1 << INT0);                                                       // Enable external interrupt INT0
+    sei();                                                                      // Enable global interrupts
     Wire.begin();
     rgb.init();
     rgb.blinkSetup(500);
@@ -19,13 +19,12 @@ void loop() {
     switchCondition();
 }
 
-void switchPressed() {
-      switchFlag = digitalRead(PIN_D2) ? false : true;
+ISR(EXT_INT0_vect) {
+    switchFlag = !bitRead(PIND, 2);
 }
 
 void switchCondition() {
-    if (switchFlag) rgb.blink(GREEN);  
+    if (switchFlag) rgb.blink(GREEN);
     else if (rgb.state(GREEN)) rgb.turnOff(GREEN);
 }
-
 
